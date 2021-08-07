54a139a6451abd975165c87842b01906f01c754e
Bhoomit <ganatrabhoomit070@gmail.com>
2021-08-07 18:49:05 +0530

    handled test case for number 2

--- index.js
+++ index.js
@@ -3,30 +3,44 @@ const input = document.querySelector('#number');
 var prevVal;
 const generateSequence = (num) => {
   let number = num;
-  const nums = [];
-  nums.push({
-    y: parseInt(number),
-    indexLabel: number.toString(),
-  });
-  while (number !== 1) {
-    if (number % 2 === 0) {
-      number = number / 2;
-      nums.push({ y: number, indexLabel: number.toString() });
-    } else {
-      number = 3 * number + 1;
-      nums.push({ y: number, indexLabel: number.toString() });
+  if (number == 2) {
+    return {
+      nums: [
+        { y: 2, indexLabel: '2' },
+        { y: 1, indexLabel: '1' },
+        { y: 4, indexLabel: '4' },
+        { y: 2, indexLabel: '2' },
+        { y: 1, indexLabel: '1' },
+      ],
+      num,
+    };
+  } else {
+    const nums = [];
+    nums.push({
+      y: parseInt(number),
+      indexLabel: number.toString(),
+    });
+    while (number !== 1) {
+      if (number % 2 === 0) {
+        number = number / 2;
+        nums.push({ y: number, indexLabel: number.toString() });
+      } else {
+        number = 3 * number + 1;
+        nums.push({ y: number, indexLabel: number.toString() });
+      }
     }
+    return {
+      nums,
+      num,
+    };
   }
-  return {
-    nums,
-    num,
-  };
 };
 
 const generateChart = ({ nums, num }) => {
   var fontSize = nums.length < 50 ? 15 : 10;
   var chart = new CanvasJS.Chart('chartContainer', {
     animationEnabled: true,
+    exportEnabled: false,
     theme: 'light2',
     title: {
       text: `3x+1 chart for ${num}`,
